e8fa83c26ee7207c81b3de933cacf483298e64df
sakshikawalkar <sakshikawalkar30@gmail.com>
2024-12-16 16:59:30 +0530

    Add files via upload

---
+++ RestapiProductsDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiProductsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiProductsDbApplication.class, args);
+	}
+
+}
